b4dc3a9830e92a08633c87337ea84e72c6df6b80
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-02-12 20:27:53 -0600

    Add files via upload
    move final print statement

--- text_extraction_tool.py
+++ text_extraction_tool.py
@@ -15,14 +15,14 @@ def extract_encoded_pdf_text(pdf_file, page_start, page_end):
     return text
 
 # scanned text extraction
-def extract_pdf_to_images(pdf_file, page_start, page_end, image_dir='X:\Files\Programming\Projects\\text_extraction_tool\images'):
+def extract_pdf_to_images(pdf_file, page_start, page_end, image_dir=r"X:/Files/Programming/Projects/text_extraction_tool/images"):
     pages = convert_from_path(pdf_file, dpi=300)
     for i, page in enumerate(pages[page_start:page_end]):
         print(page)
         image_path = f"{image_dir}/page{i + page_start}.jpg"
         page.save(image_path, 'JPEG')
 
-def extract_text_from_image(image_dir='X:\Files\Programming\Projects\\text_extraction_tool\images'):
+def extract_text_from_image(image_dir=r'X:/Files/Programming/Projects/text_extraction_tool/images'):
     text = ''
     for image_file in os.listdir(image_dir):
         if image_file.endswith('.jpg'):
@@ -55,7 +55,8 @@ def save_text_to_file(chunks, filename):
 # processing 
 def extraction_method():
     valid_options = ['scanned','encoded']
-    user_input = input("Enter extraction method: 'scanned' or 'encoded' ")
+    user_input = 'scanned' #input("Enter extraction method: 'scanned' or 'encoded' ")
+    return user_input
     while user_input not in valid_options:
         print("Enter either encoded or scanned.")
         user_input = input("Enter extraction method: 'scanned' or 'encoded' ")
@@ -63,17 +64,16 @@ def extraction_method():
 
 # user input
 def user_input(extraction_method):
-    pdf_file = input("Enter the PDF's directory': ")
-    page_start = int(input("Enter the starting page number: "))
-    page_end = int(input("Enter the ending page number: "))
-    chunk_size = int(input("Enter the desired chunk size (in words): "))
-    filename = input("Enter the text file name: ")
+    pdf_file = 'examples/test.pdf' #input("Enter the PDF's directory': ")
+    page_start = 30 #int(input("Enter the starting page number: "))
+    page_end = 60 #int(input("Enter the ending page number: "))
+    chunk_size = 400 #int(input("Enter the desired chunk size (in words): "))
+    filename = 'test' #input("Enter the text file name: ")
 
     text = ''
     if extraction_method == 'encoded':
         try:
             text = extract_encoded_pdf_text(pdf_file, page_start, page_end)
-            print('Your file has been sent to your local directory.')
         except:
             print("File could not be found.")
     elif extraction_method == 'scanned':
@@ -81,11 +81,11 @@ def user_input(extraction_method):
             extract_pdf_to_images(pdf_file, page_start, page_end)
             for page_num in range(page_start, page_end):
                 text += extract_text_from_image(page_num=page_num)
-            print('Your file has been sent to your local directory.')
         except: 
             print("File could not be found.")
     chunks = split_text(text, chunk_size)
     save_text_to_file(chunks, filename) 
+    print('Your file has been sent to your local directory.')
 
 
 # program
